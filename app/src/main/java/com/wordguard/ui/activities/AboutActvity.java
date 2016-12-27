b3208868e49c79a9eb43d12f69de028698fd8fd8
or <orshnizelon@gmail.com>
2016-12-27 11:31:02 +0200

    searchview

---
+++ app/src/main/java/com/wordguard/ui/activities/AboutActvity.java
@@ -0,0 +1,15 @@
+package com.wordguard.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wordguard.R;
+
+public class AboutActvity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_actvity);
+    }
+}
--- app/src/main/java/com/wordguard/ui/activities/MainActivity.java
+++ app/src/main/java/com/wordguard/ui/activities/MainActivity.java
@@ -1,6 +1,8 @@
 package com.wordguard.ui.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
@@ -90,6 +92,9 @@ public class MainActivity extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.m_search);
+
         return true;
     }
 
@@ -101,7 +106,7 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.m_search) {
             return true;
         }
 
@@ -110,7 +115,7 @@ public class MainActivity extends AppCompatActivity
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
@@ -119,8 +124,9 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_trash) {
 
         } else if (id == R.id.nav_settings) {
-
+            startActivity(new Intent(this, SettingsActivity.class));
         } else if (id == R.id.nav_about) {
+            startActivity(new Intent(this, AboutActvity.class));
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
