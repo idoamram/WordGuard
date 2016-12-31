a694b98ffe143b3c5ad7223722bb4b3c139778a2
or <orshnizelon@gmail.com>
2016-12-31 17:31:22 +0200

    added deadline

--- app/src/main/java/com/io/wordguard/ui/activities/AddWordActivity.java
+++ app/src/main/java/com/io/wordguard/ui/activities/AddWordActivity.java
@@ -1,27 +1,39 @@
 package com.io.wordguard.ui.activities;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.os.Build;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.text.format.DateFormat;
 import android.transition.TransitionManager;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.io.wordguard.R;
 import com.io.wordguard.ui.transitions.FabTransform;
 import com.io.wordguard.ui.transitions.MorphTransform;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 public class AddWordActivity extends AppCompatActivity {
     private ViewGroup mContainer;
     private AutoCompleteTextView mDescription;
     private Button mAdd;
+    private Calendar mCalendar;
+    private TextView mDeadline;
+    private long mDeadlineLong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +69,39 @@ public class AddWordActivity extends AppCompatActivity {
                 mAdd.setEnabled(isDescriptionValid());
             }
         });
+
+        mCalendar = Calendar.getInstance();
+
+        mDeadline = (TextView) findViewById(R.id.deadline);
+        mDeadline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Open dialog picker with today's date
+                DatePickerDialog datePickerDialog = new DatePickerDialog(AddWordActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
+                                // Get dates from dialog picker
+                                mCalendar.set(Calendar.YEAR, year);
+                                mCalendar.set(Calendar.MONTH, monthOfYear);
+                                mCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                                // Ignore the time
+                                mCalendar.set(Calendar.HOUR_OF_DAY, 0);
+                                mCalendar.set(Calendar.MINUTE, 0);
+                                mCalendar.set(Calendar.SECOND, 0);
+                                mCalendar.set(Calendar.MILLISECOND, 0);
+                                // Update text
+                                mDeadline.setText(DateFormat.getDateFormat(
+                                        getApplicationContext()).format(mCalendar.getTime()));
+                                // Save start date long
+                                mDeadlineLong = mCalendar.getTimeInMillis();
+                            }
+                        }, mCalendar.get(Calendar.YEAR), mCalendar.get(Calendar.MONTH), mCalendar.get(Calendar.DAY_OF_MONTH));
+                // Set minimum date to now
+                datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis() - 1000);
+                datePickerDialog.show();
+            }
+        });
     }
 
     public void dismiss(View view) {
@@ -73,7 +118,8 @@ public class AddWordActivity extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             TransitionManager.beginDelayedTransition(mContainer);
         }
-        Toast.makeText(this, mDescription.getText().toString(), Toast.LENGTH_LONG).show();
+        Toast.makeText(this, mDescription.getText().toString()
+                + "\n" + mDeadlineLong, Toast.LENGTH_LONG).show();
         dismiss(null);
     }
 
