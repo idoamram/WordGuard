3298bb58eb8e56dd6a20374ebbe7181c606e8edb
or <orshnizelon@gmail.com>
2017-01-02 23:32:27 +0200

    fixed swipe look

--- app/src/main/java/com/io/wordguard/ui/util/SwipeHelperCallback.java
+++ app/src/main/java/com/io/wordguard/ui/util/SwipeHelperCallback.java
@@ -1,14 +1,15 @@
 package com.io.wordguard.ui.util;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.RectF;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
+import android.util.TypedValue;
 import android.view.View;
 
 import com.io.wordguard.R;
@@ -16,13 +17,13 @@ import com.io.wordguard.R;
 
 public class SwipeHelperCallback extends ItemTouchHelper.SimpleCallback {
 
-    SwipeCallback swipeCallback;
-    Context context;
+    private SwipeCallback mSwipeCallback;
+    private Context mContext;
 
-    public SwipeHelperCallback(Context context,int dragDirs, int swipeDirs, SwipeCallback swipeCallback) {
+    public SwipeHelperCallback(Context mContext, int dragDirs, int swipeDirs, SwipeCallback mSwipeCallback) {
         super(dragDirs, swipeDirs);
-        this.swipeCallback = swipeCallback;
-        this.context = context;
+        this.mSwipeCallback = mSwipeCallback;
+        this.mContext = mContext;
     }
 
     @Override
@@ -34,41 +35,54 @@ public class SwipeHelperCallback extends ItemTouchHelper.SimpleCallback {
     public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
         int position = viewHolder.getAdapterPosition();
 
-        if (direction == ItemTouchHelper.LEFT){
-            swipeCallback.onSwiped(false, position);
+        if (direction == ItemTouchHelper.LEFT) {
+            mSwipeCallback.onSwiped(false, position);
         } else {
-            swipeCallback.onSwiped(true, position);
+            mSwipeCallback.onSwiped(true, position);
         }
     }
 
-    @Override
     public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
-
-        Bitmap icon;
-        Paint paint = new Paint();
-
-        if(actionState == ItemTouchHelper.ACTION_STATE_SWIPE){
-
+        if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
             View itemView = viewHolder.itemView;
-            float height = (float) itemView.getBottom() - (float) itemView.getTop();
-            float width = height / 3;
 
-            if (dX > 0){
-                paint.setColor(Color.parseColor("#4CAF50"));
-                RectF background = new RectF((float) itemView.getLeft(), (float) itemView.getTop(), dX,(float) itemView.getBottom());
-                c.drawRect(background,paint);
-                icon = BitmapHelper.getBitmapFromVectorDrawable(context, R.drawable.ic_done_white);
-                RectF icon_dest = new RectF((float) itemView.getLeft() + width ,(float) itemView.getTop() + width,(float) itemView.getLeft()+ 2*width,(float)itemView.getBottom() - width);
-                c.drawBitmap(icon,null,icon_dest,paint);
+            Paint paint = new Paint();
+            Bitmap icon;
+
+            if (dX > 0) {
+                icon = BitmapHelper.getBitmapFromVectorDrawable(mContext, R.drawable.ic_done_white);
+                paint.setColor(ContextCompat.getColor(mContext, R.color.quantum_green_700));
+                // Draw Rect with varying right side, equal to displacement dX
+                c.drawRect((float) itemView.getLeft(), (float) itemView.getTop(), dX,
+                        (float) itemView.getBottom(), paint);
+                // Set the image icon for Left swipe
+                c.drawBitmap(icon,
+                        (float) itemView.getLeft() + dp2px(16),
+                        (float) itemView.getTop() + ((float) itemView.getBottom() -
+                                (float) itemView.getTop() - icon.getHeight()) / 2, paint);
             } else {
-                paint.setColor(Color.parseColor("#F44336"));
-                RectF background = new RectF((float) itemView.getRight() + dX, (float) itemView.getTop(),(float) itemView.getRight(), (float) itemView.getBottom());
-                c.drawRect(background,paint);
-                icon = BitmapHelper.getBitmapFromVectorDrawable(context, R.drawable.ic_delete_white);
-                RectF icon_dest = new RectF((float) itemView.getRight() - 2*width ,(float) itemView.getTop() + width,(float) itemView.getRight() - width,(float)itemView.getBottom() - width);
-                c.drawBitmap(icon,null,icon_dest,paint);
+                icon = BitmapHelper.getBitmapFromVectorDrawable(mContext, R.drawable.ic_delete_white);
+                paint.setColor(ContextCompat.getColor(mContext, R.color.quantum_red_600));
+                // Draw Rect with varying left side, equal to the item's right side
+                // plus negative displacement dX
+                c.drawRect((float) itemView.getRight() + dX, (float) itemView.getTop(),
+                        (float) itemView.getRight(), (float) itemView.getBottom(), paint);
+                // Set the image icon for Right swipe
+                c.drawBitmap(icon,
+                        (float) itemView.getRight() - dp2px(16) - icon.getWidth(),
+                        (float) itemView.getTop() + ((float) itemView.getBottom() -
+                                (float) itemView.getTop() - icon.getHeight()) / 2, paint);
             }
+            // Fade out the view as it is swiped out of the parent's bounds
+            final float alpha = 1.0f - Math.abs(dX) / (float) viewHolder.itemView.getWidth();
+            viewHolder.itemView.setAlpha(alpha);
+            viewHolder.itemView.setTranslationX(dX);
+        } else {
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
         }
-        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+    }
+
+    private static int dp2px(int dp) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, Resources.getSystem().getDisplayMetrics());
     }
 }
