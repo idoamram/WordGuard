e28f8ecb4a531f0fc61f2e81880961fe7f8c8a3b
or <orshnizelon@gmail.com>
2017-01-02 23:55:35 +0200

    layout improvements

--- app/src/main/java/com/io/wordguard/ui/util/SwipeCallback.java
+++ app/src/main/java/com/io/wordguard/ui/util/SwipeCallback.java
@@ -1,8 +1,5 @@
 package com.io.wordguard.ui.util;
 
-/**
- * Created by Ido on 1/2/2017.
- */
 public interface SwipeCallback {
     void onSwiped(boolean isSwipedRight, int itemPosition);
 }
--- app/src/main/java/com/io/wordguard/ui/util/SwipeHelperCallback.java
+++ app/src/main/java/com/io/wordguard/ui/util/SwipeHelperCallback.java
@@ -3,7 +3,6 @@ package com.io.wordguard.ui.util;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.support.v4.content.ContextCompat;
@@ -13,7 +12,6 @@ import android.util.TypedValue;
 import android.view.View;
 
 import com.io.wordguard.R;
-
 
 public class SwipeHelperCallback extends ItemTouchHelper.SimpleCallback {
 
@@ -51,32 +49,32 @@ public class SwipeHelperCallback extends ItemTouchHelper.SimpleCallback {
 
             if (dX > 0) {
                 icon = BitmapHelper.getBitmapFromVectorDrawable(mContext, R.drawable.ic_done_white);
-                paint.setColor(ContextCompat.getColor(mContext, R.color.quantum_green_700));
+                paint.setColor(ContextCompat.getColor(mContext, R.color.swipe_done_green));
                 // Draw Rect with varying right side, equal to displacement dX
                 c.drawRect((float) itemView.getLeft(), (float) itemView.getTop(), dX,
                         (float) itemView.getBottom(), paint);
                 // Set the image icon for Left swipe
                 c.drawBitmap(icon,
-                        (float) itemView.getLeft() + dp2px(16),
+                        (float) itemView.getLeft() + dp2px(24),
                         (float) itemView.getTop() + ((float) itemView.getBottom() -
                                 (float) itemView.getTop() - icon.getHeight()) / 2, paint);
             } else {
                 icon = BitmapHelper.getBitmapFromVectorDrawable(mContext, R.drawable.ic_delete_white);
-                paint.setColor(ContextCompat.getColor(mContext, R.color.quantum_red_600));
+                paint.setColor(ContextCompat.getColor(mContext, R.color.swipe_delete_red));
                 // Draw Rect with varying left side, equal to the item's right side
                 // plus negative displacement dX
                 c.drawRect((float) itemView.getRight() + dX, (float) itemView.getTop(),
                         (float) itemView.getRight(), (float) itemView.getBottom(), paint);
                 // Set the image icon for Right swipe
                 c.drawBitmap(icon,
-                        (float) itemView.getRight() - dp2px(16) - icon.getWidth(),
+                        (float) itemView.getRight() - dp2px(24) - icon.getWidth(),
                         (float) itemView.getTop() + ((float) itemView.getBottom() -
                                 (float) itemView.getTop() - icon.getHeight()) / 2, paint);
             }
             // Fade out the view as it is swiped out of the parent's bounds
-            final float alpha = 1.0f - Math.abs(dX) / (float) viewHolder.itemView.getWidth();
-            viewHolder.itemView.setAlpha(alpha);
-            viewHolder.itemView.setTranslationX(dX);
+//            final float alpha = 1.0f - Math.abs(dX) / (float) viewHolder.itemView.getWidth();
+//            viewHolder.itemView.setAlpha(alpha);
+//            viewHolder.itemView.setTranslationX(dX);
         } else {
             super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
         }
