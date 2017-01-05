4f247fecc7d05355bf9d19e75120b792539b73a1
or <orshnizelon@gmail.com>
2017-01-05 12:06:47 +0200

    view word layout

--- app/src/main/java/com/io/wordguard/ui/activities/ViewWordActivity.java
+++ app/src/main/java/com/io/wordguard/ui/activities/ViewWordActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import com.io.wordguard.R;
 import com.io.wordguard.db.Word;
@@ -16,6 +17,8 @@ public class ViewWordActivity extends AppCompatActivity {
 
     public static final String EXTRA_WORD = "extra_word";
     private Word mWord;
+    private TextView mViewType, mViewDescription, mViewDeadline, mViewLocation,
+            mViewContactName, mViewContactNumber, mViewContactEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +28,14 @@ public class ViewWordActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         mWord = getIntent().getParcelableExtra(EXTRA_WORD);
+
+        mViewType = (TextView) findViewById(R.id.word_view_type_title);
+        mViewDescription = (TextView) findViewById(R.id.word_view_description_title);
+        mViewDeadline = (TextView) findViewById(R.id.word_view_deadline_title);
+        mViewLocation = (TextView) findViewById(R.id.word_view_location_title);
+        mViewContactName = (TextView) findViewById(R.id.word_view_contact_name_title);
+        mViewContactNumber = (TextView) findViewById(R.id.word_view_contact_number_title);
+        mViewContactEmail = (TextView) findViewById(R.id.word_view_contact_email_title);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
