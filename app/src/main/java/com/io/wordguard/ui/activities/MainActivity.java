4bf108ebd7654d45e98bd3aa29b042c38f5a7af3
or <orshnizelon@gmail.com>
2017-02-20 18:22:14 +0200

    minor changes

--- app/src/main/java/com/io/wordguard/ui/activities/MainActivity.java
+++ app/src/main/java/com/io/wordguard/ui/activities/MainActivity.java
@@ -5,14 +5,12 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import com.io.wordguard.BaseActivity;
